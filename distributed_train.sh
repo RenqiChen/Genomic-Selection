590a48718b248968dd757f1a64f80a0b59c2fe78
RenqiChen <chenrenqi@pjlab.org.cn>
2024-07-10 22:58:57 +0800

    update code

--- distributed_train.sh
+++ distributed_train.sh
@@ -1,6 +1,6 @@
 for num in {1..5}
 do
-    CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 main_wheat.py \
+    CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 main.py \
     --dataset_task_name "wheat.sterilspike1" \
     --which_k $num \
     --batch_size 116 \
